d9326f9c7b45f4bf786c53767018ecc11d2af2f6
ameyraut <amey.raut23@gmail.com>
2013-02-28 17:07:34 +0530

    Update jquery.vAlign.js

--- jquery.vAlign.js
+++ jquery.vAlign.js
@@ -1,6 +1,6 @@
 /*global jQuery */
 /**
-  * This jQuery plugin to display HTMl element vercally center align.
+	* This jQuery plugin to display HTMl element vercally center align.
 	* Amey Raut	
 	* vAlign.js 1.0
 	* Date: Thu Feb 28 14:23:00 2013  
@@ -8,39 +8,40 @@
 
 
 (function($){ 
-		  	// Extend jQuery 
-			$.fn.extend({  
-				vAlign: function() { 
+		// Extend jQuery 
+		$.fn.extend({  
 					
-					//variable to calulate height of child container
-					var eleHeight; 
+			vAlign: function() { 
 					
-					//Iterate over the current set of matched elements 
-					return this.each(function() {
+			//variable to calulate height of child container
+			var eleHeight; 
+					
+			//Iterate over the current set of matched elements 
+			return this.each(function() {
 											  
-							 // Store the object
-							 var $this = $(this); 
+			 // Store the object
+			var $this = $(this); 
 							 
-							 // Resizer() realignt element based on the element height divided by 2			
-							 var Resizer = function (){								 
+			// Resizer() realignt element based on the element height divided by 2			
+			var Resizer = function (){								 
 								  
-								 eleHeight = $this.innerHeight()/2;
+				 eleHeight = $this.innerHeight()/2;
 								 
-								 // Add CSS properties to element 
-								 $this.css({
-										top : '50%',
-										position:'relative',
-										marginTop:'-'+eleHeight+"px"
-								 });
-							 }
-							 
-							 // Call once to set.
-							 Resizer();
-							 
-							 // Call on window resize.				 
-							 $(window).on('resize', Resizer);
-					});
+					 // Add CSS properties to element 
+					 $this.css({
+						top : '50%',
+						position:'relative',
+						marginTop:'-'+eleHeight+"px"
+					 });
 				}
+							 
+				// Call once to set.
+				Resizer();
+							 
+				// Call on window resize.				 
+				$(window).on('resize', Resizer);
 			});
+		}
+	});
 		 
-		})(jQuery) 
+})(jQuery) 
